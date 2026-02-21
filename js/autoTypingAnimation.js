e0342dae4d7fd22a44651ef60d439376b20d7951
anushka18-10a <work.anushkamandwade@gmail.com>
2026-02-21 13:33:59 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Sahil Patil</span> a passionate Full Stack Developer who enjoys building scalable, user-focused web applications from concept to deployment. I specialize in both frontend and backend development, creating seamless digital experiences that are fast, responsive, and secure.  <br><br> With experience in modern technologies like React, Node.js, Express, and databases such as MongoDB and MySQL, I bridge the gap between design and functionality. I focus on writing clean, maintainable code and continuously improving my skills to stay aligned with industry best practices."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
